96590015247da583e995c57bfe470c6618758882
= <=>
2023-10-30 20:09:35 +0200

    new fracty

--- src/js/Views/recipeView.js
+++ src/js/Views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import fracty from 'fracty';
 import View from './View.js';
 
 class RecipeView extends View {
@@ -112,7 +112,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''
+      ingredient.quantity ? fracty(ingredient.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
         <span class="recipe__unit">${ingredient.unit}</span>
